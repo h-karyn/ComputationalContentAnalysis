9ea18ab4c40bdc9e5201a12cb1c080b2e32838a4
h-karyn <muhua.huang@foxmail.com>
2024-02-27 09:02:26 -0600

    completed q4

---
+++ python_notebooks/hw8/experiment_questions.py
@@ -0,0 +1,24 @@
+input1 = (f"Option 1: apple"
+f"Option 2: orange"  # 2 - this is the correct answer
+f"Option 3: watermelon"
+f"Reference: iiiiii")
+
+input2 = (f"Option 1: usa"  # 1 - this is the correct answer
+f"Option 2: canada"
+f"Option 3: Uruguay"
+f"Reference: erw")
+
+input3 = (f"Option 1: one"
+f"Option 2: three"
+f"Option 3: eleven"  # 3 - this is the correct answer
+f"Reference: yrdghj")
+
+input4 = (f"Option 1: morning"
+f"Option 2: evening"
+f"Option 3: night"  # 3 - this is the correct answer
+f"Reference: sfhu7")
+
+input5 = (f"Option 1: California"
+f"Option 2: New York" # 2 - this is the correct answer
+f"Option 3: Texas"
+f"Reference: hellohey")
